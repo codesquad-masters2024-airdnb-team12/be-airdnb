42a78568ec91fb904cc610294268c311426ea105
sharpie1330 <amberaplin14@gmail.com>
2024-06-10 01:47:37 +0900

    [feat/#4] :adhesive_bandage: fix: Address 클래스 필드명 일부 변경 및 새 필드 추가
    - 한국 우편주소형태 뿐만 아니라 전세계 우편주소 모두 입력 가능하도록 수정

--- src/main/java/com/airbnb/domain/common/Address.java
+++ src/main/java/com/airbnb/domain/common/Address.java
@@ -1,12 +1,38 @@
 package com.airbnb.domain.common;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Embeddable
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class Address {
 
+    @Column(nullable = false)
     private String country;
+
+    @Column(nullable = false)
     private String zipcode;
+    private String district;    // 면, 읍, 구, 군, 시명 (district)
+
+    @Column(nullable = false)
+    private String region;      // 시, 도명 (city/province)
+
+    @Column(nullable = false)
     private String address;
     private String detailedAddress;
+
+    @Builder
+    private Address(String country, String zipcode, String district, String region, String address, String detailedAddress) {
+        this.country = country;
+        this.zipcode = zipcode;
+        this.district = district;
+        this.region = region;
+        this.address = address;
+        this.detailedAddress = detailedAddress;
+    }
 }
