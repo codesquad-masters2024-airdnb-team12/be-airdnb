5085d789cf44c14b7d17821c94f1d4f319bf8b2a
sharpie1330 <amberaplin14@gmail.com>
2024-06-11 02:49:09 +0900

    [feat/#4] :adhesive_bandage: fix: AccommodationHashtag가 새로운 엔티티인지 확인할 수 있도록 Persistable 인터페이스 구현

--- src/main/java/com/airbnb/domain/accommodationHashtag/AccommodationHashtag.java
+++ src/main/java/com/airbnb/domain/accommodationHashtag/AccommodationHashtag.java
@@ -4,11 +4,17 @@ import com.airbnb.domain.accommodation.entity.Accommodation;
 import com.airbnb.domain.hashtag.entity.Hashtag;
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.domain.Persistable;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class AccommodationHashtag {
+@EntityListeners(AuditingEntityListener.class)
+public class AccommodationHashtag implements Persistable<AccommodationHashtagId> {
 
     @EmbeddedId
     private AccommodationHashtagId id;
@@ -23,10 +29,18 @@ public class AccommodationHashtag {
     @JoinColumn(name = "hashtag_id", nullable = false)
     private Hashtag hashtag;
 
+    @CreatedDate
+    private LocalDateTime createdAt;
+
     @Builder
     private AccommodationHashtag(Accommodation accommodation, Hashtag hashtag) {
         this.accommodation = accommodation;
         this.hashtag = hashtag;
         this.id = new AccommodationHashtagId(accommodation.getId(), hashtag.getId());
+    }
+
+    @Override
+    public boolean isNew() {
+        return createdAt == null;
     }
 }
