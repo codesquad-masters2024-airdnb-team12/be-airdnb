ab4017e00828d4fad79dbe0fec5403f0bc590dc5
ehpark <silbroeh@gmail.com>
2024-06-14 19:00:59 +0900

    [feat/#6] 🐛 bugfix: 회원가입 서비스 메서드 Transactional 어노테이션 추가
    - h2 db를 사용했을 때 정상 동작되나, mysql db를 사용했을 경우 오류 발생

--- src/main/java/com/airbnb/domain/member/service/MemberService.java
+++ src/main/java/com/airbnb/domain/member/service/MemberService.java
@@ -6,9 +6,10 @@ import com.airbnb.domain.member.dto.response.MemberListResponse;
 import com.airbnb.domain.member.dto.response.MemberResponse;
 import com.airbnb.domain.member.entity.Member;
 import com.airbnb.domain.member.repository.MemberRepository;
-import com.airbnb.global.security.PasswordEncoder;
+import com.airbnb.global.auth.jwt.JwtUtil;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -19,7 +20,9 @@ public class MemberService {
 
     private final MemberRepository memberRepository;
     private final PasswordEncoder passwordEncoder;
+    private final JwtUtil jwtUtil;
 
+    @Transactional
     public MemberResponse signUp(SignUpRequest signUpRequest) {
         if (memberRepository.existsByEmail(signUpRequest.getEmail())) {
             throw new IllegalArgumentException("동일한 이메일이 이미 존재합니다.");
