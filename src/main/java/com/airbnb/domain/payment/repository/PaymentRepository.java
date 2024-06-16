e47c5b21c2d4d27282418fc8e0c160a4d5f187b7
ehpark <silbroeh@gmail.com>
2024-06-17 01:53:41 +0900

    [feat/#12] ✨ feat: 결제 정보 조회 기능 구현
    - 게스트/호스트별 조회
    - 결제 목록 dto 생성: 목록과 더불어 통계 정보 추가

--- src/main/java/com/airbnb/domain/payment/repository/PaymentRepository.java
+++ src/main/java/com/airbnb/domain/payment/repository/PaymentRepository.java
@@ -1,10 +1,14 @@
 package com.airbnb.domain.payment.repository;
 
 import com.airbnb.domain.payment.entity.Payment;
+import com.airbnb.domain.payment.entity.PaymentStatus;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PaymentRepository extends JpaRepository<Payment, Long> {
 
+    List<Payment> findByBookingGuestIdAndStatus(Long guestId, PaymentStatus status);
+    List<Payment> findByBookingAccommodationHostIdAndStatus(Long hostId, PaymentStatus status);
 }
--- src/main/java/com/airbnb/domain/payment/service/PaymentService.java
+++ src/main/java/com/airbnb/domain/payment/service/PaymentService.java
@@ -3,10 +3,13 @@ package com.airbnb.domain.payment.service;
 import static com.airbnb.domain.booking.entity.BookingStatus.CONFIRMED;
 
 import com.airbnb.domain.payment.dto.request.PaymentExecuteRequest;
+import com.airbnb.domain.payment.dto.response.PaymentListResponse;
 import com.airbnb.domain.payment.dto.response.PaymentResponse;
 import com.airbnb.domain.payment.entity.Card;
 import com.airbnb.domain.payment.entity.Payment;
+import com.airbnb.domain.payment.entity.PaymentStatus;
 import com.airbnb.domain.payment.repository.PaymentRepository;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,5 +28,17 @@ public class PaymentService {
         targetPayment.getBooking().changeStatus(CONFIRMED);
 
         return PaymentResponse.from(targetPayment);
+    }
+
+    public PaymentListResponse getAllByGuestIdAndStatus(Long guestId, PaymentStatus status) {
+        List<Payment> payments = paymentRepository.findByBookingGuestIdAndStatus(guestId, status);
+
+        return PaymentListResponse.from(payments);
+    }
+
+    public PaymentListResponse getAllByHostIdAndStatus(Long hostId, PaymentStatus status) {
+        List<Payment> payments = paymentRepository.findByBookingAccommodationHostIdAndStatus(hostId, status);
+
+        return PaymentListResponse.from(payments);
     }
 }
